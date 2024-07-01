928c8ead65a6d043fdb43eec67033f5d3e1e955c
Stephen Werronen <stephen.werronen@gmail.com>
2024-07-01 16:38:36 -0700

    Add sections for forecast days

--- src/pages/ForecastPage.tsx
+++ src/pages/ForecastPage.tsx
@@ -1,13 +1,22 @@
 import "./ForecastPage.scss";
-import { getDate } from "../utils/calculate-dates";
+import { getDate, daysToMilliseconds } from "../utils/calculate-dates";
 
 const ForecastPage = () => {
   return (
     <div className="forecast">
       <h1 className="forecast__heading">Asteroid Forecast</h1>
       <p className="forecast__date-range">
-        {`${getDate(0).toLocaleDateString()} - ${getDate(6).toLocaleDateString()}`}
+        {`${getDate(0).toLocaleDateString()} - ${getDate(
+          6
+        ).toLocaleDateString()}`}
       </p>
+      <div className="forecast__week">
+        {[...Array(7).keys()].map((value) => (
+          <section className="forecast__day" key={daysToMilliseconds(value)}>
+            <h3>{getDate(value).toDateString()}</h3>
+          </section>
+        ))}
+      </div>
     </div>
   );
 };
