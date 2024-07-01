473a1c544233a1bc37bbd9b14285ab14f2bd8998
Stephen Werronen <stephen.werronen@gmail.com>
2024-07-01 14:01:53 -0700

    Add site logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import asteroidLogo from "../assets/images/asteroid-logo.png";
 
 import "./Header.scss";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="site-header">
+      <img
+        src={asteroidLogo}
+        alt="asteroid forecast logo"
+        className="site-header__logo"
+        onClick={() => navigate("/")}
+      />
       <h2 className="site-header__heading">
         <Link to="/" className="site-header__link">
           Asteroid Forecast
