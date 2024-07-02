a10b229456bbf7ab5b7ee13cead7c817baa2caef
Stephen Werronen <stephen.werronen@gmail.com>
2024-07-02 11:11:55 -0700

    Implement GET /asteroids api request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 import "./App.scss";
 
@@ -8,7 +10,24 @@ import ForecastPage from "./pages/ForecastPage";
 import AsteroidPage from "./pages/AsteroidPage";
 import ErrorPage from "./pages/ErrorPage";
 
+const { VITE_API_BASE_URL } = import.meta.env;
+
 function App() {
+  const [_asteroids, _setAsteroids] = useState({});
+
+  useEffect(() => {
+    const fetchAsteroids = async () => {
+      try {
+        const response = await axios.get(`${VITE_API_BASE_URL}/asteroids`);
+        console.log(response.data.near_earth_objects);
+      } catch (err) {
+        console.error("GET request to /asteroids endpoint failed: ", err);
+      }
+    }
+
+    fetchAsteroids();
+  }, []);
+
   return (
     <BrowserRouter>
       <Header />
