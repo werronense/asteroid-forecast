f73f348486f0cf99f794e226df99fa202eea609e
Stephen Werronen <stephen.werronen@gmail.com>
2024-07-11 09:18:12 -0700

    Restructure ForecastItem component

--- src/components/ForecastItem.tsx
+++ src/components/ForecastItem.tsx
@@ -18,14 +18,21 @@ export const ForecastItem: React.FC<ForecastItemProps> = ({
   return (
     <article className="forecast-item">
       <h3 className="forecast-item__heading">{heading}</h3>
-      <div className="forecast-item__asteroid-counts">
-        <p className="forecast-item__text">{asteroids.length} asteroids</p>
-        {hazardousAsteroids.length > 0 && (
-          <p className="forecast-item__text forecast-item__text--danger">
-            {hazardousAsteroids.length} of them dangerous
-          </p>
-        )}
-        <AsteroidList asteroids={asteroids} />
+      <div className="forecast-item__container">
+        <section className="forecast-item__asteroid-counts">
+          <div className="forecast-item__group">
+            <p className="forecast-item__text">{asteroids.length} asteroids</p>
+            {hazardousAsteroids.length > 0 && (
+              <p className="forecast-item__text forecast-item__text--danger">
+                {hazardousAsteroids.length} of them dangerous
+              </p>
+            )}
+          </div>
+          {/* todo: add icon button */}
+        </section>
+        <section className="forecast-item__list">
+          <AsteroidList asteroids={asteroids} />
+        </section>
       </div>
     </article>
   );
