af6adc7868d70ef4bf9610b0c7de512a67b4e9ac
Stephen Werronen <stephen.werronen@gmail.com>
2024-07-14 13:45:31 -0700

    Style approach list data

--- src/components/ApproachItem.tsx
+++ src/components/ApproachItem.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { CloseApproachData } from "../interfaces/asteroid";
+import { formatMillionsOfKms } from "../utils/format-distances";
+import "./ApproachItem.scss";
 
 interface ApproachItemProps {
   approach: CloseApproachData;
 }
 
 export const ApproachItem: React.FC<ApproachItemProps> = ({ approach }) => {
+  const { kilometers } = approach.miss_distance;
+
   return (
-    <li>
-      <span>{approach.close_approach_date}</span>
-      <span>{approach.miss_distance.kilometers}</span>
+    <li className="approach-item">
+      <h4 className="approach-item__heading">{approach.close_approach_date}</h4>
+      <span className="bold">Approach distance</span>:<br />
+      <span>{formatMillionsOfKms(kilometers)}</span>
     </li>
   );
 };
